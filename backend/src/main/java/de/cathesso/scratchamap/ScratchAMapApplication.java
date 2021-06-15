c825c503e7cbe1d4bdc34d531275ec92b8416921
Fabian F <h-f.fromme@posteo.de>
2021-06-15 11:30:25 +0200

    Init Project

---
+++ backend/src/main/java/de/cathesso/scratchamap/ScratchAMapApplication.java
@@ -0,0 +1,13 @@
+package de.cathesso.scratchamap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScratchAMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScratchAMapApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/de/cathesso/scratchamap/ScratchAMapApplicationTests.java
@@ -0,0 +1,13 @@
+package de.cathesso.scratchamap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScratchAMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

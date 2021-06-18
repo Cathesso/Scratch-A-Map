9501c08d0bc33669e48ce4c98ee8b38211e18c2a
Fabian F <h-f.fromme@posteo.de>
2021-06-18 12:05:00 +0200

    Cleanup

--- frontend/src/pages/ExplorePage.js
+++ frontend/src/pages/ExplorePage.js
@@ -1,6 +1,5 @@
 import styled from "styled-components/macro";
 import StreetMap from "../components/StreetMap";
-import Header from "../components/Header";
 
 export default function ExplorePage() {
   return (

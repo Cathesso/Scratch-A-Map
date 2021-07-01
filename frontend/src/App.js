a53ec71915c60c62a7a65885d7bf61ca56c666c6
Fabian F <h-f.fromme@posteo.de>
2021-07-01 13:12:32 +0200

    Add Background and Change Login-Design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import AuthProvider from "./context/AuthProvider";
 import HomePage from "./pages/HomePage";
 import { useState } from "react";
+import background from "./img/background.jpg";
 
 function App() {
   const [points, setPoints] = useState(0);
@@ -37,6 +38,8 @@ const Wrapper = styled.div`
   height: 100%;
   width: 100vw;
   background-color: hotpink;
+  background-image: url(${background});
+  background-size: cover;
   display: flex;
   flex-direction: column;
 `;
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -21,38 +21,40 @@ export default function LoginPage() {
 
   return (
     <Wrapper>
-      <Form onSubmit={handleSubmit}>
-        <div className="canvas">
-          <div className="screen">
-            <div className="cursor" />
-            <div className="eyes" />
-            <div className="mouth" />
+      <div class="centerpiece">
+        <Form onSubmit={handleSubmit}>
+          <div className="canvas">
+            <div className="screen">
+              <div className="cursor" />
+              <div className="eyes" />
+              <div className="mouth" />
+            </div>
+            <div className="case" />
           </div>
-          <div className="case" />
-        </div>
 
-        <TextField
-          label="Username"
-          defaultValue="Username"
-          variant="outlined"
-          name="username"
-          onChange={handleChange}
-          value={credentials.username}
-        />
+          <TextField
+            label="Username"
+            defaultValue="Username"
+            variant="outlined"
+            name="username"
+            onChange={handleChange}
+            value={credentials.username}
+          />
 
-        <TextField
-          label="Password"
-          defaultValue="password"
-          variant="outlined"
-          type="password"
-          name="password"
-          onChange={handleChange}
-          value={credentials.password}
-        />
-        <Button variant="contained" color="primary" type="submit">
-          Login
-        </Button>
-      </Form>
+          <TextField
+            label="Password"
+            defaultValue="password"
+            variant="outlined"
+            type="password"
+            name="password"
+            onChange={handleChange}
+            value={credentials.password}
+          />
+          <Button variant="contained" color="primary" type="submit">
+            Login
+          </Button>
+        </Form>
+      </div>
     </Wrapper>
   );
 }
@@ -66,6 +68,13 @@ const Wrapper = styled.div`
 
   .MuiFormControl-root {
     margin: 10px 0;
+  }
+
+  .centerpiece {
+    grid-column-start: 2;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
 
   .canvas {
@@ -165,13 +174,37 @@ const Wrapper = styled.div`
 `;
 
 const Form = styled.form`
-  grid-column-start: 2;
+  color: white;
+  background: rgba(128, 128, 128, 0.35);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(9.5px);
+  -webkit-backdrop-filter: blur(9.5px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
   display: flex;
   flex-direction: column;
-  justify-items: center;
+  justify-content: center;
   align-items: center;
+  padding: 10px;
 
   button {
     width: 30%;
   }
 `;
+
+/*
+.login{
+ padding:2em;
+ width: min(30rem,80%);
+ min-height:150px;
+ display:flex;
+ flex-direction:column;
+ justify-content:center;
+
+background: rgba( 5, 0, 0, 0.35 );
+box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+backdrop-filter: blur( 9.5px );
+-webkit-backdrop-filter: blur( 9.5px );
+border-radius: 10px;
+border: 1px solid rgba( 255, 255, 255, 0.18 );
+*/

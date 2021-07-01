9950b6a861ae6d4eedd303c51202be26c68b9fa8
Fabian F <h-f.fromme@posteo.de>
2021-07-01 14:22:20 +0200

    Change Security

--- backend/src/main/java/de/cathesso/scratchamap/security/config/SecurityConfig.java
+++ backend/src/main/java/de/cathesso/scratchamap/security/config/SecurityConfig.java
@@ -45,8 +45,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers("/auth/login").permitAll()
                 .antMatchers("/**").authenticated()
+                .antMatchers("/").permitAll()
                 .and().addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
